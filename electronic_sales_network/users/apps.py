f9e56ecb51b037c16cfaa300c6b0d2bfc2124941
Amir <99445613+freddy1911@users.noreply.github.com>
2023-05-18 02:42:37 +0300

    init

---
+++ electronic_sales_network/users/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'electronic_sales_network.users'
+    verbose_name = 'Пользователи'
---
+++ electronic_sales_network/users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    """
+    Модель 'Пользователь'. Наследуется от AbstractUser.
+    """
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
